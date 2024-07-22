e36cedcf600393b80e81307e89e6aa0a46f28e10
willlockwood <wmlckwd@gmail.com>
2024-07-22 09:39:52 -0700

    Add library-level doc comment and re-order library exports

--- lib/sturdy_http.dart
+++ lib/sturdy_http.dart
@@ -1,8 +1,9 @@
+/// A strongly typed, event-based, reliable HTTP client that wraps Dio.
 library sturdy_http;
 
 export 'src/_dio_error.dart';
-export 'src/sturdy_http.dart';
-export 'src/sturdy_http_event_listener.dart';
 export 'src/network_request.dart';
 export 'src/network_response.dart';
 export 'src/retry_behavior.dart';
+export 'src/sturdy_http.dart';
+export 'src/sturdy_http_event_listener.dart';
